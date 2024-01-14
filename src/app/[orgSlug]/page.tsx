3da526ae33abba43ff250512d42fddea5f521da6
Guru Swarupa <112751363+guruswarupa@users.noreply.github.com>
2024-01-14 16:55:38 +0530

    validate admin access to organizations. users cannot access all organizations.

--- src/app/[orgSlug]/page.tsx
+++ src/app/[orgSlug]/page.tsx
@@ -1,19 +1,61 @@
 "use client";
-
+import { useParams, notFound } from "next/navigation";
+import { useEffect, useState } from "react";
+import { supabaseClient } from "@/utils/supabaseClient";
+import { LoaderAtomic } from "@/components/utils/loader";
 import { SessionStore } from "@/store/session";
-import { notFound, useParams } from "next/navigation";
-
 export default function OrganizationHomePage() {
+  const params = useParams<{ orgSlug: string }>();
+  const [organizationExists, setOrganizationExists] = useState(true);
+  const [loading, setLoading] = useState(true);
   const { dbUser, orgSlug } = SessionStore();
 
-  const params = useParams<{ orgSlug: string }>();
-
-  if (params.orgSlug === orgSlug || dbUser?.isAdmin) {
-    // TODO: if user is admin. and the org doesnt exist the show Not found
-    return <div>ProjectListing</div>;
+  if (!dbUser?.isAdmin) {
+    if (params.orgSlug !== orgSlug) {
+      notFound();
+    }
   }
 
-  notFound();
+  useEffect(() => {
+    const fetchOrganization = async () => {
+      const sessionResponse = await supabaseClient.auth.getSession();
+      const currentSession = sessionResponse.data?.session;
+
+      if (!currentSession) {
+        setLoading(false);
+        return;
+      }
+
+      const organizationQuery = await supabaseClient
+        .from("Organization")
+        .select("slug")
+        .eq("slug", params.orgSlug)
+        .single();
+
+      if (!organizationQuery.data) {
+        console.log("Organization not found!");
+        setOrganizationExists(false);
+      }
+
+      setLoading(false);
+    };
+
+    fetchOrganization();
+  }, [params.orgSlug]);
+
+  if (loading) {
+    return (
+      <div className="absolute top-0 left-0 w-screen h-screen flex justify-center items-center">
+        <LoaderAtomic />
+      </div>
+    );
+  }
+
+  if (!organizationExists) {
+    notFound();
+  }
+
+  return <div>Project Listing for Organization {params.orgSlug}</div>;
 }
 
 // ListingPage---- (data, newCompProps, atomicCard, onAdd, onSucces, onEerr)
